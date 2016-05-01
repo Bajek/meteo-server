26082283ec0c832eb7be3636437d9afa47be2a90
Bajek <bartek.urbaniec@gmail.com>
2016-05-01 17:38:52 +0200

    Init  commit - maven structure

---
+++ src/main/java/pl/ubytes/MeteoServerApplication.java
@@ -0,0 +1,12 @@
+package pl.ubytes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeteoServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeteoServerApplication.class, args);
+	}
+}
---
+++ src/test/java/pl/ubytes/MeteoServerApplicationTests.java
@@ -0,0 +1,18 @@
+package pl.ubytes;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = MeteoServerApplication.class)
+@WebAppConfiguration
+public class MeteoServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
